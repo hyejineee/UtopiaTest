2537a887b0ad5a307fe19899fe01bb068f97682d
hyejineee <dev.hyejin04@gmail.com>
2021-01-30 02:07:08 +0900

    첫 번째 질문 화면 구현

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
+
+
+const gifPlayTime = [9000,15000,14000,8000,8000,7000,11000,14000,12000]
+
+const score = {
+    type1 : 0,
+    type2 : 0,
+    type3 : 0,
+    type4 : 0,
+}
+
 const showButtonBox = ()=>{
+    document.getElementById("background-image").src = "./images/1.png"
     let box = document.getElementsByClassName("button-box")[0]
     box.style.display= "block"
 }
 
 
 const callback = ()=>{
-    console.log("hellp")
-    setTimeout(()=>showButtonBox(), 14000)
+    const imgIndex = document.getElementById('background-image').getAttribute('value')*1-1
+    setTimeout(()=>showButtonBox(), gifPlayTime[imgIndex])
 }
 
+const handleClickTypeButton = ()=>{
+    
+}
+
+const handleClickNextButton = ()=>{
+    
+}
