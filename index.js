34eb6d50a32156d0343713c9b5e48ca51a62a203
hyejineee <dev.hyejin04@gmail.com>
2021-02-02 13:38:14 +0900

    Implement result page

--- index.js
+++ index.js
@@ -15,11 +15,7 @@ const showButtonBox = (questionIndex)=>{
     box.style.display= "block"
 }
 
-
-
-const callback = ()=>{
-
-    console.log('callback')
+const onLoadHandler = ()=>{
     
     const image = document.getElementById('background-image')
     const regex = /([^\s]+(?=\.(png)))/
@@ -32,12 +28,59 @@ const callback = ()=>{
     setTimeout(()=>showButtonBox(questionIndex), gifPlayTime[questionIndex-1])
 }
 
+const gridStyle = () =>{
+    let box = document.getElementsByClassName("button-box")[0]
+   
+    box.style.top ="30%"
 
+    const ul = box.getElementsByTagName('ul')[0]
+    ul.style.display ="grid"
+    ul.style.gridTemplateColumns = "1fr 1fr"
+    ul.style.gridTemplateRows = "1fr 1fr"
+
+    const img = ul.getElementsByTagName('img')
+
+    for(let i =0; i< img.length; i++){
+        img[i].style.width ="95%"
+    }
+}
+
+const clearGridStyle = ()=>{
+
+    let box = document.getElementsByClassName("button-box")[0]
+    box.style.top ="50%"
+
+    const ul = box.getElementsByTagName('ul')[0]
+    ul.style.display ="block"
+
+    const img = ul.getElementsByTagName('img')
+
+    for(let i =0; i< img.length; i++){
+        img[i].style.width ="100%"
+    }
+}
+
+const showResultImage = ()=>{
+    const [key,] = Object.entries(score)
+    .sort(([,a],[,b]) => a-b)[3]
+    
+    document.getElementById("background-image").src = `./images/${key}.png`
+    let box = document.getElementsByClassName("button-box")[0]
+    box.style.display= "none"
+}
 
 const handleClickTypeButton = (type)=>{
+
+
     const image = document.getElementById('background-image')
 
     const nextQuestionIndex = image.getAttribute('value')*1+1
+
+    if(nextQuestionIndex === 9){
+        //결과 페이지로 이동 
+        showResultImage()
+        return
+    }
 
     if((nextQuestionIndex-1) !== 8) {
         score[type] += 1
@@ -46,12 +89,20 @@ const handleClickTypeButton = (type)=>{
     image.src = `./images/${nextQuestionIndex}.webp`
     image.setAttribute('value', nextQuestionIndex)
 
-    const buttons = document.getElementsByClassName('button-image')[0]
+    const buttons = document.getElementsByClassName('button-image')
 
-    Array(buttons).forEach((element, i) => {
-        element.src = `./images/buttons/question${nextQuestionIndex}buttons/button_${i}.png`
-    });
+    for(let i =0; i<buttons.length; i++){
+        buttons[i].src = `./images/buttons/question${nextQuestionIndex}buttons/button_${i}.png`
+    }
 
     let box = document.getElementsByClassName("button-box")[0]
     box.style.display= "none"
+
+    if(nextQuestionIndex === 5 || nextQuestionIndex === 8){
+        gridStyle()
+        return 
+    }
+    clearGridStyle()
 }
+
+
